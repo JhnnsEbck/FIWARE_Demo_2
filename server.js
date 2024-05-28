99ac94e2f8bc640eb902cb741637d5da26bce1a1
Johannes Eiboeck <96199305+JhnnsEbck@users.noreply.github.com>
2024-05-28 15:57:32 +0200

    WL API Request Funktion

--- server.js
+++ server.js
@@ -16,51 +16,66 @@ const NASA_API_URL = "https://data.nasa.gov/resource/gh4g-9sfh.json";
 const ORION_URL = "http://localhost:1026/ngsi-ld/v1/entities";
 const NGSI_LD_CONTEXT = "https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld";
 
-function fetch_nasa_data() {
-    return fetch(NASA_API_URL)
-        .then(response => response.json());
+
+//Wiener Linien API Request
+const WL_API_REQ ="https://www.wienerlinien.at/ogd_realtime/monitor?diva=60200500&activateTrafficInfo=stoerunglang&activateTrafficInfo=aufzugsinfo";
+
+fetch_wl_station();
+function fetch_wl_station(/*stationDIVA*/){
+    return fetch(WL_API_REQ)
+        .then(response => response.json())
+        .then(data => console.log(data.data.monitors[0]))
+        .catch(error => console.log('Error:', error));
 }
 
-function transform_to_ngsi(nasa_data) {
-    const ngsi_entities = [];
-    for (const item of nasa_data) {
-        if ('geolocation' in item && 'latitude' in item['geolocation'] && 'longitude' in item['geolocation']) {
-            const ngsi_entity = {
-                "id": `urn:ngsi-ld:Asteroid:${item['id']}`,
-                "type": "Asteroid",
-                "@context": NGSI_LD_CONTEXT,
-                "name": {
-                    "type": "Property",
-                    "value": item['name'] || 'Unknown'
-                },
-                "mass": {
-                    "type": "Property",
-                    "value": item['mass'] || 'Unknown'
-                },
-                "fall": {
-                    "type": "Property",
-                    "value": item['fall'] || 'Unknown'
-                },
-                "year": {
-                    "type": "Property",
-                    "value": item['year'] || 'Unknown'
-                },
-                "location": {
-                    "type": "GeoProperty",
-                    "value": {
-                        "type": "Point",
-                        "coordinates": [
-                            parseFloat(item['geolocation']['longitude']),
-                            parseFloat(item['geolocation']['latitude'])
-                        ]
-                    }
-                }
-            };
-            ngsi_entities.push(ngsi_entity);
-        }
-    }
-    return ngsi_entities;
-}
+
+
+
+// function fetch_nasa_data() {
+//     return fetch(NASA_API_URL)
+//         .then(response => response.json());
+// }
+
+// function transform_to_ngsi(nasa_data) {
+//     const ngsi_entities = [];
+//     for (const item of nasa_data) {
+//         if ('geolocation' in item && 'latitude' in item['geolocation'] && 'longitude' in item['geolocation']) {
+//             const ngsi_entity = {
+//                 "id": `urn:ngsi-ld:Asteroid:${item['id']}`,
+//                 "type": "Asteroid",
+//                 "@context": NGSI_LD_CONTEXT,
+//                 "name": {
+//                     "type": "Property",
+//                     "value": item['name'] || 'Unknown'
+//                 },
+//                 "mass": {
+//                     "type": "Property",
+//                     "value": item['mass'] || 'Unknown'
+//                 },
+//                 "fall": {
+//                     "type": "Property",
+//                     "value": item['fall'] || 'Unknown'
+//                 },
+//                 "year": {
+//                     "type": "Property",
+//                     "value": item['year'] || 'Unknown'
+//                 },
+//                 "location": {
+//                     "type": "GeoProperty",
+//                     "value": {
+//                         "type": "Point",
+//                         "coordinates": [
+//                             parseFloat(item['geolocation']['longitude']),
+//                             parseFloat(item['geolocation']['latitude'])
+//                         ]
+//                     }
+//                 }
+//             };
+//             ngsi_entities.push(ngsi_entity);
+//         }
+//     }
+//     return ngsi_entities;
+// }
 
 async function send_or_update_entity(entity) {
     const entity_id = entity['id'];
@@ -123,21 +138,21 @@ async function send_to_orion(ngsi_data) {
     }
 }
 
-async function fetch_and_send_data_to_orion() {
-    while (true) {
-        try {
-            const nasa_data = await fetch_nasa_data();
-            const ngsi_data = transform_to_ngsi(nasa_data);
-            await send_to_orion(ngsi_data);
-            await new Promise(resolve => setTimeout(resolve, 3600000)); // Fetch data every hour, adjust as needed (3600000 milliseconds)
-        } catch (error) {
-            console.error(`Error: ${error.message}`);
-        }
-    }
-}
+// async function fetch_and_send_data_to_orion() {
+//     while (true) {
+//         try {
+//             const nasa_data = await fetch_nasa_data();
+//             const ngsi_data = transform_to_ngsi(nasa_data);
+//             await send_to_orion(ngsi_data);
+//             await new Promise(resolve => setTimeout(resolve, 3600000)); // Fetch data every hour, adjust as needed (3600000 milliseconds)
+//         } catch (error) {
+//             console.error(`Error: ${error.message}`);
+//         }
+//     }
+// }
 
 // Start the data fetching
-fetch_and_send_data_to_orion().catch(error => console.error(`Error: ${error.message}`));
+//fetch_and_send_data_to_orion().catch(error => console.error(`Error: ${error.message}`));
 
 // HTTP server part
 const server = http.createServer((req, res) => {
